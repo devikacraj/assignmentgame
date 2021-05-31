73f60c3318baed2654825bd98237e8adae0f8ac3
DEVIKA C RAJ <76911590+devikacraj@users.noreply.github.com>
2021-05-31 17:19:10 +0530

    Update Game1.java

--- Game1.java
+++ Game1.java
@@ -1,7 +1,7 @@
 package pattern;
 import java.util.Random;
 import java.util.Scanner;
-public class Main {
+public class Game1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
